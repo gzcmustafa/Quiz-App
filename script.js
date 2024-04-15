d7c9de9cec9c81cde8d07fc1979df4bfd87b45a9
Mustafa Gözcü <gozcumustafa34@gmail.com>
2024-04-15 12:47:07 +0300

    feat: add Quiz Constructor

--- script.js
+++ script.js
@@ -10,6 +10,28 @@ function Soru(soruMetni,cevapSecenekleri,dogruCEvap) {
 let soru1 = new Soru ("JavaScript paket yönetim uygulaması nedir ?",{a:"Node.js",b:"Typescript",c:"npm"},"c")
 let soru2 = new Soru (".Net paket yönetim uygulaması nedir ?",{a:"Node.js",b:"Nuget",c:"NPM"},"b")
 
+let sorular = [
+    new Soru ("1-JavaScript paket yönetim uygulaması nedir ?",{a:"Node.js",b:"Typescript",c:"npm"},"c"),
+    new Soru ("2-JavaScript paket yönetim uygulaması nedir ?",{a:"Node.js",b:"Typescript",c:"npm"},"c"),
+    new Soru ("3-JavaScript paket yönetim uygulaması nedir ?",{a:"Node.js",b:"Typescript",c:"npm"},"c"),
+    new Soru ("4-JavaScript paket yönetim uygulaması nedir ?",{a:"Node.js",b:"Typescript",c:"npm"},"c")
+]
 
-console.log(soru1.soruMetni)
-console.log(soru1.dogruCEvap)
+function Quiz(sorular){
+    this.sorular = sorular;
+    this.soruIndex = 0;
+}
+Quiz.prototype.soruGetir = function (){
+    return this.sorular[this.soruIndex]
+}
+const quiz = new Quiz(sorular);
+
+document.querySelector(".btn-start").addEventListener("click",function(){
+    if(quiz.sorular.length != quiz.soruIndex){
+        console.log(quiz.soruGetir());
+        quiz.soruIndex += 1;
+    }else{
+        console.log("quiz bitti")
+    }
+})
+
