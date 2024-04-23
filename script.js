0fd6267caeaca95fe5c5f1582515b7a6b83da1cf
Mustafa Gözcü <gozcumustafa34@gmail.com>
2024-04-23 16:41:28 +0300

    feat: Add control for answer options

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ document.querySelector(".btn_start").addEventListener("click",function(){
     
         document.querySelector(".quiz_box").classList.add("active");
         soruGoster(quiz.soruGetir())
+        document.querySelector(".next_btn").classList.remove("show")
     
 })
 
@@ -34,12 +35,15 @@ document.querySelector(".next_btn").addEventListener("click",function(){
     if(quiz.sorular.length != quiz.soruIndex + 1){
         quiz.soruIndex += 1;
         soruGoster(quiz.soruGetir())
+        document.querySelector(".next_btn").classList.remove("show")
     }else{
         console.log("quiz bitti")
     }
 
 })
-
+const opiton_list = document.querySelector(".option_list")
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
 function soruGoster(soru){
     let question = `<span>${soru.soruMetni}</span>`;
     let options = '';
@@ -52,8 +56,34 @@ function soruGoster(soru){
            </div>
         `;
     }
+
     document.querySelector(".question_text").innerHTML = question;
-    document.querySelector(".option_list").innerHTML = options;
+    opiton_list.innerHTML = options;
+
+    const option = opiton_list.querySelectorAll(".option");
+    
+    for (let opt of option){
+        opt.setAttribute("onclick","optionSelected(this)")
+    }
+}
+
+function optionSelected(option){
+    let cevap = option.querySelector("span b").textContent;
+    let soru = quiz.soruGetir();
+
+    if(soru.cevabiKontrolEt(cevap)){
+        option.classList.add("correct")
+        option.insertAdjacentHTML("beforeend",correctIcon)
+    } else {
+        option.classList.add("incorrect");
+        option.insertAdjacentHTML("beforeend",incorrectIcon)
+
+    }
+
+    for ( let i = 0; i<opiton_list.children.length; i++) {
+        opiton_list.children[i].classList.add("disabled")
+    }
+    document.querySelector(".next_btn").classList.add("show")
 }
 
 
